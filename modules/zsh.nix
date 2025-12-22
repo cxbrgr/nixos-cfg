f8500d53bba3f92d9dcf5e0affa8f84ad2173553
chr-ber <john@doe.pamine>
2025-12-22 14:53:14 +0100

    fix zsh

--- modules/zsh.nix
+++ modules/zsh.nix
@@ -1,16 +1,34 @@
 { config, lib, ... }:
 let
-  enabled = config.illogical-impulse.enable &&
-   config.programs.zsh.enable;
+  enabled = config.illogical-impulse.enable;
 in
 {
   config = lib.mkIf enabled {
-    programs.zsh.initExtra = ''
-      # Use the generated color scheme
+    programs.zsh = {
+      enable = true;
+      enableCompletion = true;
+      autosuggestion.enable = true;
+      syntaxHighlighting.enable = true;
+      
+      shellAliases = {
+        ll = "ls -l";
+        c = "clear";
+        g = "git";
+        gs = "git status";
+        flake-update = "~/dotfiles/scripts/flake-update.bash";
+        flake-rebuild = "sudo nixos-rebuild switch --flake ~/dotfiles#nixos && hyprctl reload";
+        flake-drybuild = "nixos-rebuild dry-build --flake ~/dotfiles#nixos";
+        flake-eval = "nix eval --raw ~/dotfiles#homeConfigurations.chrisleebear.activationPackage";
+        flake-eval-verbose = "nix eval --json .#nixosConfigurations.nixos.config.home-manager.users.chrisleebear.home.packages --apply 'pkgs: map (p: p.pname or p.name) pkgs' | jq -r '.[]' | sort | uniq";
+      };
 
-      if test -f ~/.cache/ags/user/generated/terminal/sequences.txt; then
-          cat ~/.cache/ags/user/generated/terminal/sequences.txt
-      fi
-    '';
+      initExtra = ''
+        # Use the generated color scheme
+
+        if test -f ~/.cache/ags/user/generated/terminal/sequences.txt; then
+            cat ~/.cache/ags/user/generated/terminal/sequences.txt
+        fi
+      '';
+     }; 
   };
 }
