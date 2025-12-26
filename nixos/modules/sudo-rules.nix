95825822bf32511e43ea5dbb4aea9186d2dc201f
chr-ber <john@doe.pamine>
2025-12-26 15:16:16 +0100

    add shutdown to sudoers

--- nixos/modules/sudo-rules.nix
+++ nixos/modules/sudo-rules.nix
@@ -8,6 +8,10 @@
         { 
           command = "/run/current-system/sw/bin/nixos-rebuild";
           options = [ "NOPASSWD" ];
+        },
+        { 
+          command = "/run/current-system/sw/bin/shutdown";
+          options = [ "NOPASSWD" ];
         }
       ];
     }
