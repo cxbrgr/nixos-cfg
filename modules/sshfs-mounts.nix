5e28f48b156e6ea23fcdeeb10f3596a559007ec0
= <=>
2026-01-18 09:29:08 +0100

    mnt rtr entwware drive on wrkstn

--- modules/sshfs-mounts.nix
+++ modules/sshfs-mounts.nix
@@ -13,6 +13,7 @@
   # Create mount point directory
   systemd.tmpfiles.rules = [
     "d /mnt/hmsrvr/data 0755 chrisleebear users -"
+    "d /mnt/rtr/entware 0755 chrisleebear users -"
   ];
 
   # Mount hmsrvr:/data using SSHFS with automount
@@ -25,16 +26,39 @@
       "x-systemd.idle-timeout=60" # Unmount after 60s idle
       "_netdev"                   # Wait for network
       "users"                     # Allow user to mount/unmount
-      "identityfile=/home/chrisleebear/.ssh/id_hmsrvr"
       "allow_other"               # Allow other users to access
       "default_permissions"       # Use local permission checking
       "reconnect"                 # Auto-reconnect on connection loss
       "ServerAliveInterval=15"    # Send keepalive every 15s
       "ServerAliveCountMax=3"     # Disconnect after 3 missed keepalives
+      "identityfile=/home/chrisleebear/.ssh/id_hmsrvr"
+    ];
+  };
+
+  fileSystems."/mnt/rtr/entware" = {
+    device = "cxbrgr@rtr:/tmp/mnt/rtr/entware";
+    fsType = "fuse.sshfs";
+    options = [
+      "noauto"                    # Don't mount at boot
+      "x-systemd.automount"       # Mount on first access
+      "x-systemd.idle-timeout=60" # Unmount after 60s idle
+      "_netdev"                   # Wait for network
+      "users"                     # Allow user to mount/unmount
+      "allow_other"               # Allow other users to access
+      "default_permissions"       # Use local permission checking
+      "reconnect"                 # Auto-reconnect on connection loss
+      "ServerAliveInterval=15"    # Send keepalive every 15s
+      "ServerAliveCountMax=3"     # Disconnect after 3 missed keepalives
+      "uid=1000"                  # Makes remote files appear owned by chrisleebear locally
+      "gid=100"                   # Makes remote files appear owned by 'users' group locally
+      "StrictHostKeyChecking=accept-new" # Accept new host keys
+      "identityfile=/home/chrisleebear/.ssh/id_rtr"
+      "UserKnownHostsFile=/home/chrisleebear/.ssh/known_hosts"
     ];
   };
 
   networking.hosts = {
     "10.10.10.10" = [ "hmsrvr" ];
+    "10.10.10.1" = [ "rtr" ];
   };
 }
