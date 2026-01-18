addf7180d0babb9500be1d2bba1c56650d504159
= <=>
2026-01-18 09:40:04 +0100

    fix wireguard

--- modules/wireguard.nix
+++ modules/wireguard.nix
@@ -1,13 +1,19 @@
-{ config, pkgs, ... }:
+{ 
+  config,
+  pkgs,
+  ... 
+}:
 {
-  # WireGuard kernel module
-  boot.extraModulePackages = [ config.boot.kernelPackages.wireguard ];
+  boot.kernelModules = [ "wireguard" ];
 
-  # WireGuard tools
   environment.systemPackages = with pkgs; [
     wireguard-tools
   ];
 
-  # Enable WireGuard in the firewall (optional - allows incoming WireGuard connections)
-  # networking.firewall.allowedUDPPorts = [ 51820 ];
+  networking.wireguard = {
+    enable = true;
+  };
+
+  # we need systemd-resolved enabled to configure split DNS
+  services.resolved.enable = true;
 }
